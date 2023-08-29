0d14493e0a29e17c46de9202c5ccc07d4966aa9e
kaesinol <keiplyer@gmail.com>
2023-08-29 11:11:14 +0800

    :bookmark::sparkles: v0.3.0 Added error processing for requests.exceptions.JSONDecodeError

--- sm_ms_api/__init__.py
+++ sm_ms_api/__init__.py
@@ -8,15 +8,21 @@ class SMMS(object):
         self.header = {"Authorization": token}
 
     def upload_image(self, path) -> str:
-        with open(path, 'rb') as file:
-            files = {'smfile': file}
-            res = requests.post(self.root + 'upload', files=files, headers=self.header).json()
-            if res['success']:
-                return res['data']['url']
-            elif res['code'] == 'image_repeated':
-                return res['images']
-            else:
-                raise ImageUploadError(res['message'])
+        err: str = ''
+        try:
+            with open(path, 'rb') as file:
+                files = {'smfile': file}
+                res = requests.post(self.root + 'upload', files=files, headers=self.header)
+                err = res.text
+                res = res.json()
+                if res['success']:
+                    return res['data']['url']
+                elif res['code'] == 'image_repeated':
+                    return res['images']
+                else:
+                    raise ImageUploadError(res['message'])
+        except requests.exceptions.JSONDecodeError:
+            raise Exception(err)
 
 
 class ImageUploadError(Exception):
